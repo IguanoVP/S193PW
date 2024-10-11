d504df2ce9b86ecd03a7758f495f2ec47392b0dc
IguanoVP <122044076@upq.edu.mx>
2024-10-10 21:10:11 -0600

    Practica numero 4

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorVista;
 
 /* Route::get('/', function () {
     return view('inicio');
@@ -13,8 +14,14 @@ Route::get('/consultar', function () {
     return view('clientes');
 })->name('rutaClientes'); */
 
-Route::view('/','inicio')->name('rutaInicio');
-Route::view('/form','formulario')->name('rutaform');
-Route::view('/consultar','clientes')->name('rutaClientes');
+//Route::view('/','inicio')->name('rutaInicio');
+//Route::view('/form','formulario')->name('rutaform');
+//Route::view('/consultar','clientes')->name('rutaClientes');
 
 Route::view('/component','componentes')->name('rutacomponent');
+
+//rutas para controlador
+
+Route::get('/',[ControladorVista::class,'home']) ->name('rutainicio');
+Route::get('/form',[ControladorVista::class,'formulario']) ->name('rutaform');
+Route::get('/consultar',[ControladorVista::class,'consulta']) ->name('rutaclientes');
